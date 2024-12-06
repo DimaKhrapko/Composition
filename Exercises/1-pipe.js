9ee558082f62ba712a183c8011cecc59ca603bf3
DimaKhrapko <khrapko2007@gmail.com>
2024-12-07 00:17:33 +0200

    Add solution

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const pipe = (...fns) => (x) => null;
+const pipe = (...fns) => {
+  for (const funcs of fns) {
+    if (typeof funcs !== 'function') {
+      throw new Error('All arguments must be functions');
+    }
+  }
+  return (x) => fns.reduce((c, f) => f(c), x);
+};
 
 module.exports = { pipe };
